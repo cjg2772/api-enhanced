1db354464fa20bed8dc70323c066fe12a2a02348
MoeFurina <noreply@focalors.ltd>
2026-06-18 22:29:24 +0800

    refactor: 优化serverless入口文件

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-require('./app.js')
+// Serverless 入口 + 本地开发通用入口
+
+const fs = require('fs')
+const path = require('path')
+const tmpPath = require('os').tmpdir()
+
+// 确保 anonymous_token 文件存在（util/request.js 加载时需要读取）
+if (!fs.existsSync(path.resolve(tmpPath, 'anonymous_token'))) {
+  fs.writeFileSync(path.resolve(tmpPath, 'anonymous_token'), '', 'utf-8')
+}
+
+const { constructServer } = require('./server')
+const generateConfig = require('./generateConfig')
+
+// Serverless 懒加载
+let app = null
+
+module.exports = async (req, res) => {
+  if (!app) {
+    await generateConfig()
+    app = await constructServer()
+  }
+  return app(req, res)
+}
+
+// Vercel判定
+if (!process.env.VERCEL) {
+  require('./app.js')
+}
